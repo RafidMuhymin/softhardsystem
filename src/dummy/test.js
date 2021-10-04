cee780f584b98833da4dad6e9b55499897e5b9aa
RafidMuhymin <rafidmuhymin@gmail.com>
2021-10-05 00:11:21 +0600

    created postlayout and toc

--- src/dummy/test.js
+++ src/dummy/test.js
@@ -1,70 +1,39 @@
-<Layout title="Soft">
-  <div class="max-w-2xl p-6 mx-auto">
-    <h1 class="flex flex-col justify-center">
-      Soft
-      <small class="text-right">{soft.length} Articles</small>
-    </h1>
-    <div class="flex flex-col gap-8 p-6">
-      {soft.map((post) => {
-        console.log(post);
-        const { title, slug, modified, uagb_excerpt } = post;
-        const { source_url, alt_text } = post._embedded["wp:featuredmedia"][0];
+import Layout from "./Layout.astro";
+import { createRequire } from "node:module";
+const moduleRequire = createRequire(import.meta.url);
+const { JSDOM } = moduleRequire("jsdom");
 
-        const dateString =
-          "`${dateArray[0]}, ${dateArray[1]} ${dateArray[2]}, ${dateArray[3]}`";
+const { title, post } = Astro.props;
 
-        const dateArray = new Date(`${modified}Z`).toDateString().split(" ");
-        const date = `${dateArray[0]}, ${dateArray[1]} ${dateArray[2]}, ${dateArray[3]}`;
-        return (
-          <section>
-            <a href={`/${slug}`}>
-              <img src={source_url} alt={alt_text} />
-            </a>
+const { document } = new JSDOM(
+  `<h2>Table Of Contents</h2>` + post.content.rendered
+).window;
 
-            <h2>
-              <a href={`/${slug}`}>{title.rendered}</a>
-            </h2>
+let toc = "";
+let level = 0;
+let headingNumber = 0;
 
-            <p>{uagb_excerpt.slice(0, -8)}</p>
+const content = document.body.innerHTML.replace(
+  /<h([\d])>([^<]+)<\/h([\d])>/gi,
+  function (str, openLevel, titleText, closeLevel) {
+    if (openLevel != closeLevel) {
+      return str;
+    }
 
-            <div class="flex flex-wrap justify-between items-center gap-2 py-4">
-              <span class="inline-block">
-                Last Updated : &nbsp;
-                <time dateTime={`${modified}Z`}>
-                  {`<script>
-                    (() => {
-                      const dateArray = new Date("${modified}Z").toDateString().split(" ");
-                      document.write(${dateString});
-                    })();
-                  </script>`}
-                  <noscript>{date}</noscript>
-                </time>
-              </span>
+    if (openLevel > level) {
+      toc += new Array(openLevel - level + 1).join("<ul>");
+    } else if (openLevel < level) {
+      toc += new Array(level - openLevel + 1).join("</ul>");
+    }
 
-              <span class="inline-block">
-                Categorized as :{" "}
-                {post._embedded["wp:term"][0].map((cat, i) => (
-                  <a href={`/category/${cat.slug}`}>
-                    {cat.name}
-                    {i === post._embedded["wp:term"][0].length - 1 ? "" : ", "}
-                  </a>
-                ))}
-              </span>
+    level = parseInt(openLevel);
+    headingNumber++;
+    toc += `<li><a href="#toc-${headingNumber}">${titleText}</a></li>`;
+    return `<h${openLevel} id="toc-${headingNumber}">${titleText}</h${closeLevel}>`;
+  }
+);
 
-              <span class="inline-block">
-                Tagged :{" "}
-                {post._embedded["wp:term"][1].map((tag, i) => (
-                  <a href={`/tag/${tag.slug}`}>
-                    {tag.name}
-                    {i === post._embedded["wp:term"][1].length - 1 ? "" : ", "}
-                  </a>
-                ))}
-              </span>
-            </div>
-            <Pushable slug={slug} />
-          </section>
-        );
-      })}
-    </div>
-  </div>
-</Layout>;
+if (level) {
+  toc += new Array(level + 1).join("</ul>");
+  toc = new JSDOM(toc).window.document.body.firstChild.innerHTML;
+}
