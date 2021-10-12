8469c644a38ff2b48546edc86702dec8f3c78975
RafidMuhymin <rafidmuhymin@gmail.com>
2021-10-12 16:19:24 +0600

    added comment but tweaked cors

---
+++ src/scripts/layout.js
@@ -0,0 +1,37 @@
+document.getElementById("menubar").onclick = () => {
+  const form = document.querySelector("#header > div form");
+  const navlinks = document.querySelectorAll("#header > div nav a");
+
+  if (checkbox.checked) {
+    navlinks.forEach((l, i) => {
+      l.animate({ left: [0, `-${300 + (navlinks.length - i) * 100}%`] }, 500);
+    });
+    form.animate({ left: [0, "-100%"] }, 500).onfinish = () => {
+      checkbox.checked = false;
+      document.body.style.overflow = "auto";
+    };
+  } else {
+    checkbox.checked = true;
+    document.body.style.overflow = "hidden";
+    form.animate({ left: ["-100%", 0] }, 400);
+    navlinks.forEach((l, i) => {
+      l.animate({ left: ["-100%", 0] }, 200 + i * 100);
+    });
+  }
+};
+
+window.onresize = () => {
+  if (window.innerWidth >= 768) {
+    checkbox.checked = false;
+    document.body.style.overflow = "auto";
+  }
+};
+
+window.onNavigate = () => {
+  checkbox.checked = false;
+  document.body.style.overflow = "auto";
+};
+
+window.onMount = () => {
+  scan();
+};
