c43da91fdc6833fa82f288291cf8770789a9483b
RafidMuhymin <rafidmuhymin@gmail.com>
2021-11-03 23:55:31 +0600

    subset itim

--- postbuild.js
+++ postbuild.js
@@ -44,3 +44,34 @@ allFiles.forEach((file) => {
 });
 
 console.log("Finished minifying CSS in", new Date() - start, "ms");
+
+(async () => {
+  const itimBuffer = Buffer.from(
+    fs.readFileSync("public/fonts/itim-v5-latin-regular.woff2")
+  );
+
+  let chars = "!";
+
+  const allFiles = getAllFiles("./dist");
+
+  allFiles.forEach((file) => {
+    const content = fs.readFileSync(file, "utf-8");
+    const { document } = new JSDOM(content).window;
+    document.querySelectorAll("em, .pacific").forEach((node) => {
+      chars += node.textContent;
+    });
+  });
+
+  const itimSubsetBuffer = await subsetFont(itimBuffer, chars, {
+    targetFormat: "woff2",
+  });
+
+  const itimBase64 =
+    "data:font/woff2;base64," + itimSubsetBuffer.toString("base64");
+
+  console.log(itimBase64.length);
+
+  fs.writeFileSync("dist/fonts/itim-v5-latin-regular.woff2", itimBase64);
+})();
+
+console.log("Finished subsetting fonts in", new Date() - start, "ms");
