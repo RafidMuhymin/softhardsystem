e7532224f3272f86f79eaf78e86dcc7831ad2af4
RafidMuhymin <rafidmuhymin@gmail.com>
2021-11-22 18:27:45 +0600

    updated the layouts folder structure

--- src/astro-spa/navigate.js
+++
@@ -1,8 +0,0 @@
-export default function () {
-  return `AstroSpa.navigate = (href, options) => {
-    if (location.href !== href && location.pathname !== href) {
-      history[options?.reload ? "replaceState" : "pushState"](options?.state || {}, document.title, href);
-      constructPage();
-    }
-  };`;
-}
