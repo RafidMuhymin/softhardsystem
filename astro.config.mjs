16d615080c767e9f2da76fbf93d5d5d7e24a6087
RafidMuhymin <rafidmuhymin@gmail.com>
2021-12-31 17:14:25 +0600

    fixed css inlining

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
+import { viteSingleFile } from "vite-plugin-singlefile";
+
 export default {
   buildOptions: {
     site: "https://softhardsystem.com/",
+  },
+  vite: {
+    plugins: [viteSingleFile()],
+    build: {
+      target: "esnext",
+      assetsInlineLimit: 100000000,
+      chunkSizeWarningLimit: 100000000,
+      cssCodeSplit: false,
+      brotliSize: false,
+      rollupOptions: {
+        inlineDynamicImports: true,
+      },
+    },
   },
 };
